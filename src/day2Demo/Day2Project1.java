23bd2eb59b718ae7601c912239ea94c3f043d25e
nasim <nasim@10.0.0.10>
2023-04-08 01:24:31 -0400

    initial part of day2 project1

---
+++ src/day2Demo/Day2Project1.java
@@ -0,0 +1,13 @@
+package day2Demo;
+
+public class Day2Project1 {
+	public static void main(String[] args) {
+		
+		System.out.println("day2 homework");
+		
+		System.out.println("test");
+		
+		
+	}
+
+}
